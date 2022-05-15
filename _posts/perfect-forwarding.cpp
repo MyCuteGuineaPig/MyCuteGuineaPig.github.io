6c8d1772423915ac6d06d0af055d31ec49d86fa7
beckswu <beckswu@gmail.com>
2022-05-14 22:47:33 -0400

    add c++

---
+++ _posts/perfect-forwarding.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class object{
+public:
+  vector<int>_v;
+  void set(const std::vector<int> & v) { 
+      cout<<" lvalue "<<endl;
+      _v = v;   
+  }
+  void set(std::vector<int> && v) { 
+    cout<<" move "<<endl;
+    _v = std::move(v); 
+  }
+  
+  //Forwarding references only work with templates
+  template<class T> //必须用template
+  void perfectSet(T && t){
+    set(std::forward<T>(t));   
+  }
+};
+
+
+
+
+int main()
+{
+    object v;
+  	v.perfectSet<vector<int>>({1,2,3,4,5}); //call lvalue
+  	vector<int> t = {1,2,3};
+  	v.perfectSet(t);  //call rvalue
+}
